8a2df967d2db254843e14f37bb7a2ab5862ef552
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-13 03:27:40 +0530

    modify something in booking model

--- src/models/booking.js
+++ src/models/booking.js
@@ -26,6 +26,16 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         values:[ "In Process","Booked", "Cancelled"],
         defaultValue: "In Process"
+      },
+      noOfSeats : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1
+      },
+      totalCost : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
       }
     },
     {
