f2b118a39f89656083eef012e9fcc81afb66cede
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-13 01:02:41 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+};
+
+setupAndStartServer();
